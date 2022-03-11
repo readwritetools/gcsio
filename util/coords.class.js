997d5ebd5b083f029b8d60d870832d9200276d08
Joe Honton <joe@joe-honton.com>
2022-03-11 09:27:12 -0800

    deploy

--- util/coords.class.js
+++ util/coords.class.js
@@ -1,12 +1,11 @@
 /* Copyright (c) 2022 Read Write Tools. */
 import expect from 'softlib/expect.js';
 
-import aver from 'softlib/aver.js';
-
 export default class Coords {
     constructor(t, e) {
-        expect(t, 'Number'), expect(e, 'Number'), this.longitude = Math.fround(t), this.latitude = Math.fround(e), 
-        aver(this.longitude >= -180 && this.longitude <= 180), aver(this.latitude >= -90 && this.latitude <= 90);
+        expect(t, 'Number'), expect(e, 'Number'), e > 90 && (e = 90), e < -90 && (e = -90), 
+        t > 180 && (t -= 360), t < -180 && (t += 360), this.longitude = Math.fround(t), 
+        this.latitude = Math.fround(e);
     }
     get mapKey() {
         return `${this.longitude.toFixed(7)},${this.latitude.toFixed(7)}`;
