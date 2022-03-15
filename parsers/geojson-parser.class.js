6d2024af900757624ad672b048dfaf20e7c82680
Joe Honton <joe@joe-honton.com>
2022-03-15 13:34:41 -0700

    deploy

--- parsers/geojson-parser.class.js
+++ parsers/geojson-parser.class.js
@@ -35,13 +35,13 @@ export default class GeojsonParser {
             return terminal.caught(e), !1;
         }
     }
-    handleAmbiguousJson(e) {
+    handleAmbiguousJson(e, t) {
         if ('Array' == e.constructor.name) for (let t = 0; t < e.length; t++) this.handleAmbiguousJson(e[t]); else if ('Object' == e.constructor.name) switch (e.type) {
           case 'FeatureCollection':
             return void this.handleFeatureCollection(e);
 
           case 'Feature':
-            return void this.handleFeature(e);
+            return void this.handleFeature(e, t);
 
           default:
             terminal.trace(`ignoring geoJSON object type ${geojsonObj.type}`);
@@ -49,36 +49,40 @@ export default class GeojsonParser {
     }
     handleFeatureCollection(e) {
         aver('FeatureCollection' == e.type), expect(e.features, 'Array');
-        for (let t = 0; t < e.features.length; t++) this.handleAmbiguousJson(e.features[t]);
+        for (let r = 0; r < e.features.length; r++) {
+            var t = r;
+            this.handleAmbiguousJson(e.features[r], t);
+        }
     }
-    handleFeature(e) {
-        aver('Feature' == e.type), expect(e.geometry, 'Object'), expect(e.properties, [ 'Object', 'null', 'undefined' ]);
-        const t = e.geometry.coordinates;
-        expect(t, 'Array');
+    handleFeature(e, t) {
+        if (aver('Feature' == e.type), expect(e.geometry, [ 'Object', 'null' ]), expect(e.properties, [ 'Object', 'null', 'undefined' ]), 
+        null == e.geometry) return void terminal.trace(`feature ${t} has no geometry, skipping`);
+        const r = e.geometry.coordinates;
+        expect(r, 'Array');
         const o = e.geometry.type;
         switch (expect(o, 'String'), o) {
           case 'Point':
-            this.handlePoint(e.properties, t);
+            this.handlePoint(e.properties, r);
             break;
 
           case 'MultiPoint':
-            for (let o = 0; o < t.length; o++) this.handlePoint(e.properties, t[o]);
+            for (let t = 0; t < r.length; t++) this.handlePoint(e.properties, r[t]);
             break;
 
           case 'LineString':
-            this.handleLine(e.properties, t);
+            this.handleLine(e.properties, r);
             break;
 
           case 'MultiLineString':
-            for (let o = 0; o < t.length; o++) this.handleLine(e.properties, t[o]);
+            for (let t = 0; t < r.length; t++) this.handleLine(e.properties, r[t]);
             break;
 
           case 'Polygon':
-            this.handlePolygon(e.properties, t);
+            this.handlePolygon(e.properties, r);
             break;
 
           case 'MultiPolygon':
-            for (let o = 0; o < t.length; o++) this.handlePolygon(e.properties, t[o]);
+            for (let t = 0; t < r.length; t++) this.handlePolygon(e.properties, r[t]);
             break;
 
           case 'GeometryCollection':
@@ -91,49 +95,49 @@ export default class GeojsonParser {
     }
     handlePoint(e, t) {
         expect(e, 'Object'), expect(t, 'Array');
-        const o = this.copyProperties(e), r = new Coords(t[0], t[1]);
-        this.createPoint(o, r);
+        const r = this.copyProperties(e), o = new Coords(t[0], t[1]);
+        this.createPoint(r, o);
     }
     createPoint(e, t) {
         expect(e, 'Object'), expect(t, 'Coords');
-        var o = new GcsPointFeature;
-        o.kvPairs = e, o.discretePoint = t, this.gcsFeaturePoints.push(o);
+        var r = new GcsPointFeature;
+        r.kvPairs = e, r.discretePoint = t, this.gcsFeaturePoints.push(r);
     }
     handleLine(e, t) {
         expect(e, 'Object'), expect(t, 'Array');
-        const o = this.copyProperties(e), r = [];
+        const r = this.copyProperties(e), o = [];
         for (let e = 0; e < t.length; e++) {
-            const o = t[e];
-            r.push(new Coords(o[0], o[1]));
+            const r = t[e];
+            o.push(new Coords(r[0], r[1]));
         }
-        this.createLine(o, r);
+        this.createLine(r, o);
     }
     createLine(e, t) {
         expect(e, 'Object'), expect(t, 'Array');
-        var o = new GcsLineFeature;
-        o.kvPairs = e, o.lineSegment = t, this.gcsFeatureLines.push(o);
+        var r = new GcsLineFeature;
+        r.kvPairs = e, r.lineSegment = t, this.gcsFeatureLines.push(r);
     }
     handlePolygon(e, t) {
         expect(e, 'Object'), expect(t, 'Array');
-        const o = this.copyProperties(e), r = new PolygonRing, s = new PolygonCutouts;
+        const r = this.copyProperties(e), o = new PolygonRing, s = new PolygonCutouts;
         for (let e = 0; e < t.length; e++) {
-            const o = t[e], a = o.length;
+            const r = t[e], a = r.length;
             if (0 == e) {
-                for (let e = 0; e < a; e++) r.push(new Coords(o[e][0], o[e][1]));
-                aver(r[0].longitude == r[a - 1].longitude), aver(r[0].latitude == r[a - 1].latitude);
+                for (let e = 0; e < a; e++) o.push(new Coords(r[e][0], r[e][1]));
+                aver(o[0].longitude == o[a - 1].longitude), aver(o[0].latitude == o[a - 1].latitude);
             } else {
                 var n = new PolygonRing;
-                for (let e = 0; e < a; e++) n.push(new Coords(o[e][0], o[e][1]));
+                for (let e = 0; e < a; e++) n.push(new Coords(r[e][0], r[e][1]));
                 aver(n[0].longitude == n[a - 1].longitude), aver(n[0].latitude == n[a - 1].latitude), 
                 s.push(n);
             }
         }
-        this.createPolygon(o, r, s);
+        this.createPolygon(r, o, s);
     }
-    createPolygon(e, t, o) {
-        expect(e, 'Object'), expect(t, 'PolygonRing'), expect(o, 'PolygonCutouts');
-        var r = new GcsPolygonFeature;
-        r.kvPairs = e, r.outerRing = t, r.innerRings = o, this.gcsFeaturePolygons.push(r);
+    createPolygon(e, t, r) {
+        expect(e, 'Object'), expect(t, 'PolygonRing'), expect(r, 'PolygonCutouts');
+        var o = new GcsPolygonFeature;
+        o.kvPairs = e, o.outerRing = t, o.innerRings = r, this.gcsFeaturePolygons.push(o);
     }
     copyProperties(e) {
         return expect(e, 'Object'), null == e || null == e ? {} : Object.assign({}, e);
